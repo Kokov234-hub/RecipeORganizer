2e5cb888bb70a3fe23f29a2016229c15a71c9e6c
Kokov234-hub <kosyo63@gmail.com>
2025-04-27 20:11:28 +0300

    премахнат файл

--- obj/Debug/net8.0/RecipeORganizer.AssemblyInfo.cs
+++ obj/Debug/net8.0/RecipeORganizer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RecipeORganizer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cee8d961da14906e058a075a64c73eeda61042b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("RecipeORganizer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RecipeORganizer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
